6fd2c2e8094c38b03733ee049f1e231df3ee8f33
huyhoangvn <huyhoang.77.got@gmail.com>
2020-12-13 23:08:44 +0700

    11h 13/12 firebase function deploy

--- functions/index.js
+++ functions/index.js
@@ -1,1 +1,16 @@
 const functions = require('firebase-functions');
+//http request -> end point request 
+exports.randomNumber = functions.https.onRequest((resquest, respone)=>{
+    const number = Math.round(Math.random() * 100);
+    respone.send(number.toString());
+})
+
+exports.redirectLink = functions.https.onRequest((resquest, respone)=>{
+    respone.redirect('http://localhost:5000');
+})
+
+//callable function (can call directly from our code)
+exports.sayHello = functions.https.onCall((data, context)=>{
+    const mes = data.mes;
+    return `Hello, ${mes}`
+})
--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,28 @@
-
 const requestModal = document.querySelector('.new-request');
 const requestLink = document.querySelector('.add-request');
+//say hello function call
+const buttonHello = document.querySelector('.call');
 
 //open request modal
 requestLink.addEventListener('click', ()=>{
-    requestModal.classList.add('open'); 
+    requestModal.classList.add('open');
 });
 
 //close request modal
 requestModal.addEventListener('click', (eventParams)=>{
-    if(eventParams.target.ClassList.contains('new-request')){
+    if(eventParams.target.classList.contains('new-request')){
         requestModal.classList.remove('open');
     }
 });
+
+buttonHello.addEventListener('click', ()=>{
+    buttonHello.classList.add('active');
+/*
+    //get function reference
+    const sayHello = firebase.function().https().httpsCallable('sayHello');
+    sayHello({mes : 'Random Reply'}).then((result) => {
+        console.log(result.data);
+    });//async/return promise
+*/
+});
+
